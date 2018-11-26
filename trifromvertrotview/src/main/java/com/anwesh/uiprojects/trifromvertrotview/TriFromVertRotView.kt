f370bcd6417cc7444cd73ebe824942f4f464432c
Anwesh43 <anweshthecool0@gmail.com>
2018-11-26 14:39:38 +0530

    adding TFVRNode class

--- trifromvertrotview/src/main/java/com/anwesh/uiprojects/trifromvertrotview/TriFromVertRotView.kt
+++ trifromvertrotview/src/main/java/com/anwesh/uiprojects/trifromvertrotview/TriFromVertRotView.kt
@@ -134,4 +134,50 @@ class TriFromVertRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TFVRNode(var i : Int, val state : State = State()) {
+
+        private var next : TFVRNode? = null
+        private var prev : TFVRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TFVRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            paint.style = Paint.Style.STROKE
+            canvas.drawTFVRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TFVRNode {
+            var curr : TFVRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+    }
 }
