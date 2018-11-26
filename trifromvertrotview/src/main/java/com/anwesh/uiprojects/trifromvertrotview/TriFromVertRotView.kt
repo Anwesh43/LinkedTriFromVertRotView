cdf02097fcb9f611115c9224abc1024b6b4c68b5
Anwesh43 <anweshthecool0@gmail.com>
2018-11-26 14:01:05 +0530

    adding Int and Float extension matches

--- trifromvertrotview/src/main/java/com/anwesh/uiprojects/trifromvertrotview/TriFromVertRotView.kt
+++ trifromvertrotview/src/main/java/com/anwesh/uiprojects/trifromvertrotview/TriFromVertRotView.kt
@@ -19,3 +19,13 @@ val sizeFactor : Int = 3
 val strokeFactor : Int = 90
 val scDiv : Double = 0.51
 val scGap : Float = 0.05f
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * scGap * dir
