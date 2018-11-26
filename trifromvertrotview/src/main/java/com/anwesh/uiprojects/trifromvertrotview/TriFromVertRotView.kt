073090099a41847f0edcb957e4e3ed2ef66e903c
Anwesh43 <anweshthecool0@gmail.com>
2018-11-26 14:28:43 +0530

    adding drawTFVRNode canvas extension method

--- trifromvertrotview/src/main/java/com/anwesh/uiprojects/trifromvertrotview/TriFromVertRotView.kt
+++ trifromvertrotview/src/main/java/com/anwesh/uiprojects/trifromvertrotview/TriFromVertRotView.kt
@@ -8,10 +8,7 @@ import android.view.View
 import android.view.MotionEvent
 import android.app.Activity
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Color
-import android.graphics.Path
+import android.graphics.*
 
 val nodes : Int = 5
 val tris : Int = 3
@@ -19,6 +16,7 @@ val sizeFactor : Int = 3
 val strokeFactor : Int = 90
 val scDiv : Double = 0.51
 val scGap : Float = 0.05f
+val color : Int = Color.parseColor("#311B92")
 
 fun Int.getInverse() : Float = 1f / this
 
@@ -29,3 +27,45 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
 
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * scGap * dir
+
+fun Float.getPointInCircle(deg : Double) : PointF = PointF(this * Math.cos(deg).toFloat(), this * Math.sin(deg).toFloat())
+
+fun Canvas.drawTriangle(r : Float, deg : Double, paint : Paint) {
+    val path : Path = Path()
+    for (j in (0..2)) {
+        val pointInCircle : PointF = r.getPointInCircle(deg/4 + deg * j)
+        if (j == 0) {
+            path.moveTo(pointInCircle.x, pointInCircle.y)
+        } else {
+            path.lineTo(pointInCircle.x, pointInCircle.y)
+        }
+    }
+    drawPath(path, paint)
+}
+
+fun Canvas.drawLineToVertex(r : Float, i : Int, deg : Double, paint : Paint) {
+    val vertex : PointF = r.getPointInCircle(deg/4 + deg * i)
+    drawLine(0f, 0f, vertex.x, vertex.y, paint)
+}
+
+fun Canvas.drawTFVRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val deg : Double = 2 * Math.PI / (tris)
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = color
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(gap * (i + 1), h / 2)
+    rotate(180f * sc2)
+    drawTriangle(size, deg, paint)
+    for (j in 0..(tris - 1)) {
+        val sc : Float = sc1.divideScale(j, tris)
+        drawLineToVertex(size * sc, j, deg, paint)
+    }
+    restore()
+}
