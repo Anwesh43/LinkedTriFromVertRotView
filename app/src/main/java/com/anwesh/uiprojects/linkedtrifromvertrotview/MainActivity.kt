03e7aa683efc4c4e310f71d4ea103503da34dca1
Anwesh43 <anweshthecool0@gmail.com>
2018-11-26 14:49:06 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtrifromvertrotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtrifromvertrotview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtrifromvertrotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.trifromvertrotview.TriFromVertRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriFromVertRotView.create(this)
     }
 }
