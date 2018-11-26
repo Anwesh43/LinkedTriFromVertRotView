427134a03e59b16107347a91ffeefeca925c8431
Anwesh43 <anweshthecool0@gmail.com>
2018-11-26 13:56:24 +0530

    adding libraries and defining constants

--- trifromvertrotview/src/main/java/com/anwesh/uiprojects/trifromvertrotview/TriFromVertRotView.kt
+++ trifromvertrotview/src/main/java/com/anwesh/uiprojects/trifromvertrotview/TriFromVertRotView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.trifromvertrotview
 /**
  * Created by anweshmishra on 26/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Path
+
+val nodes : Int = 5
+val tris : Int = 3
+val sizeFactor : Int = 3
+val strokeFactor : Int = 90
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
