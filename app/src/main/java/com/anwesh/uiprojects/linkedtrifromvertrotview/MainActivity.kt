6c95b1debced6abdcf4b76e882caeed5ca1d040b
Anwesh43 <anweshthecool0@gmail.com>
2018-11-26 14:50:37 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedtrifromvertrotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtrifromvertrotview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedtrifromvertrotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.trifromvertrotview.TriFromVertRotView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TriFromVertRotView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
